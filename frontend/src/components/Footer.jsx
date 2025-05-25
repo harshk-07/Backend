886d0990bb8c6596e352138ebb0532def0a260b9
harshk-07 <harshk.pro@gmail.com>
2025-05-25 18:02:55 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -26,8 +26,8 @@ const Footer = () => {
         </svg>
       </div>
 
-      <div className="container mx-auto px-4 pt-20 pb-8">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+      <div className="container mx-20 px-4 pt-20 pb-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           <div className="space-y-4">
             <h3 className="text-2xl font-bold text-blue-200">❤️ LifeShare</h3>
             <p className="text-gray-300">
